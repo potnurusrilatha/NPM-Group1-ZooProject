566e678cf58be58bf7deca6423afa906a2b23b90
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-02-10 21:57:05 +0100

    uploading the changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+import express from "express";
+import * as path from "path";
+
+const PORT = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/",(req,res) => {
+    res.send("Hello this is from NPM project");
+})
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
