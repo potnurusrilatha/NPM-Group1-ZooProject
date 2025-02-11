4d431294c04bb6cdf5b865a662dbcc44a93df1e1
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-02-11 11:35:54 +0100

    uploading the file

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import * as path from "path";
+import mammalsRouter from "./routes/mammals_router.js";
+import { animalArray } from "./data/animals.js";
+
 
 const PORT = 3000;
 const app = express();
+const __dirname = path.resolve();
 
+app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
 
 app.get("/",(req,res) => {
-    res.send("Hello this is from NPM project");
+    res.render("pages/home.ejs",{
+        welcome_text : "home",
+        animal_names : animalArray
+    })
 })
+
+app.use("/mammals",mammalsRouter);
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
