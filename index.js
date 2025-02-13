44422008ea092ef84481436b05ac7acaa092feea
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2025-02-13 14:30:10 +0100

    added perentie animal

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as path from "path";
 import { animalArray } from "./data/animals.js";
+import reptilesRouter from "./routes/reptiles_router.js";
 
 
 
@@ -17,12 +18,13 @@ app.get("/",(req,res) => {
     console.log(animalArray.length)
     res.render("pages/home.ejs",{
         animal : animalArray,
-       main_content_page :"home",
+        main_content_page :"home",
         body_class:"home",
         welcome_text : "Australian Zoo",
         animal_names : animalArray
     })
 })
 
+app.use("/reptiles",reptilesRouter)
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
--- routes/reptiles_router.js
+++ routes/reptiles_router.js
@@ -1,56 +1,41 @@
 import express from "express";
-import {reptileArray } from "../data/animals.js";
+import { animalArray, reptileArray } from "../data/animals.js";
 
 
 const reptilesRouter = express();
 
 reptilesRouter.get("/",(req,res) => {
-    res.render('pages/home',{
+    res.render('pages/home.ejs',{
+        body_class: "home",
+        header_class : "home",
+        main_content_page : "home",
         welcome_text : "reptiles",
+        style_file_name: "home",
         animal_names : reptileArray
     })
 })
 
- reptilesRouter.get("/Frill-necked lizard",(req,res) => {
-//     let frillNeckedLizard = reptileArray[0];
-//     res.render('pages/animal_display.ejs', {
-//          animal_name: animal_name.name,
+reptilesRouter.get("/Frillneckedlizard",(req,res)  => {
+    let frillneckedlizard = reptileArray[1];
+    res.render("pages/animal_display.ejs", {
+        animal: frillNeckedLizard,
+        main_content_page: "animals",
+        welcome_text: "reptiles",
+        animal_names:reptileArray
 
-//     })
+    })
+
 })
 
-// reptilesRouter.get("/hawksbillTurtle" ,(req,res) => {
-//     let hawksbillTurtle  = reptileArray[1];
-//     res.render("pages/animal_display.ejs", {
-//         animal_name: hawksbillTurtle.name,
-//         group_name : hawksbillTurtle.groupName,
-//         lifeSpan: hawksbillTurtle.lifeSpan,
-//         food: hawksbillTurtle.food,
-//         description: hawksbillTurtle.description,
-//         length: hawksbillTurtle.length,
-//         weight: hawksbillTurtle.weight,
-//         place: hawksbillTurtle.place,
-//         src: hawksbillTurtle.src,
-//         group: hawksbillTurtle.group,
-//         groupLink: hawksbillTurtle.groupLink
-//     })
-// })
+ reptilesRouter.get("/perentie" ,(req,res) => {
+    let perentie  = reptileArray[2];
+    res.render("pages/animal_display.ejs", {
+        animal: perentie,        
+        main_content_page : "animals",
+        welcome_text : "reptiles",
+        animal_names: reptileArray
 
-// reptilesRouter.get("/perentie" ,(req,res) => {
-//     let perentie  = reptileArray[2];
-//     res.render("pages/animal_display.ejs", {
-//         animal_name: perentie.name,
-//         group_name : perentie.groupName,
-//         lifeSpan: perentie.lifeSpan,
-//         food: perentie.food,
-//         description: perentie.description,
-//         length: perentie.length,
-//         weight: perentie.weight,
-//         place: perentie.place,
-//         src: perentie.src,
-//         group: perentie.group,
-//         groupLink: perentie.groupLink
-//     })
-// })
+    })
+ })
 
 export default reptilesRouter;
