3819d2fd2c385845e51c462bc54f374b4700adab
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2025-02-15 23:08:39 +0100

    deleted mammals router from index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import * as path from "path";
 import { animalArray } from "./data/animals.js";
-import mammalsRouter  from "./routes/mammals_router.js";
 import birdsRouter from "./routes/birds_router.js";
 import mammalsRouter  from "./routes/mammals_router.js";
 import reptilesRouter from "./routes/reptiles_router.js";
